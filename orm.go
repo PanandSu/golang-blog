416fd6ce0a173a1d3ffd45b18f65f13371ad8359
PanJinhao <2158870977@qq.com>
2024-03-09 14:37:35 +0800

    gorm

--- orm.go
+++ orm.go
@@ -6,9 +6,10 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
+	"strconv"
 )
 
-func init() {
+func main() {
 	username := "root"
 	password := "(PanandSu521125)"
 	host := "127.0.0.1:3306"
@@ -39,11 +40,120 @@ func init() {
 		return
 	}
 	r := gin.Default()
+	r.POST("/user/add", func(c *gin.Context) {
+		var user List
+		err = c.ShouldBindJSON(user)
+		if err != nil {
+			c.JSON(200, gin.H{
+				"code": 400,
+				"msg":  err.Error(),
+				"data": nil,
+			})
+		} else {
+			db.Create(&user)
+			c.JSON(200, gin.H{
+				"code": 200,
+				"msg":  "ok",
+				"data": user,
+			})
+		}
+	})
+	r.DELETE("/user/delete/:id", func(c *gin.Context) {
+		var users []List
+		id := c.Param("id")
+		db.Where("id =?", id).Find(&users)
+		if len(users) == 0 {
+			c.JSON(200, gin.H{
+				"code": 400,
+				"msg":  "id 没有找到,删除失败",
+			})
+		} else {
+			db.Where("id = ?", id).Delete(&users)
+			c.JSON(200, gin.H{
+				"code": 200,
+				"msg":  "删除成功",
+			})
+		}
+	})
+	r.POST("/user/update/:id", func(c *gin.Context) {
+		var user List
+		id := c.Param("id")
+		db.Where("id = ?", id).Find(&user)
+		if user.ID == 0 {
+			c.JSON(200, gin.H{
+				"code": 400,
+				"msg":  "用户id没有找到",
+			})
+		} else {
+			err = c.ShouldBindJSON(&user)
+			if err != nil {
+				c.JSON(200, gin.H{
+					"code": 400,
+					"msg":  "修改失败",
+				})
+			} else {
+				db.Where("id = ?", id).Updates(&user)
+				c.JSON(200, gin.H{
+					"code": 200,
+					"msg":  "修改成功",
+					"data": user,
+				})
+			}
+		}
+	})
+	r.GET("/user/list/:id", func(c *gin.Context) {
+		var users []List
+		id := c.Param("id")
+		db.Where("id =?", id).Find(&users)
+		if len(users) == 0 {
+			c.JSON(200, gin.H{
+				"code": 400,
+				"msg":  "没有找到该用户",
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"code": 200,
+				"msg":  "ok",
+				"data": users,
+			})
+		}
+	})
+	r.GET("/user/list", func(c *gin.Context) {
+		var users []List
+		pageSize, _ := strconv.Atoi(c.Query("pageSize"))
+		pageNum, _ := strconv.Atoi(c.Query("pageNum"))
+		if pageSize == 0 {
+			pageSize--
+		}
+		if pageNum == 0 {
+			pageNum--
+		}
+		offset := (pageNum - 1) * pageSize
+		if pageSize == -1 && pageNum == -1 {
+			offset = -1
+		}
+		db.Model(&users).Limit(pageSize).Offset(offset).Find(&users)
+		if len(users) == 0 {
+			c.JSON(200, gin.H{
+				"code": 400,
+				"msg":  "没有查询到数据",
+				"data": gin.H{},
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"code": 200,
+				"msg":  "查询成功",
+				"data": gin.H{
+					"list":     users,
+					"pageNum":  pageNum,
+					"pageSize": pageSize,
+					"total":    len(users),
+				},
+			})
+		}
+	})
 	port := "8083"
 	_ = r.Run(":" + port)
-}
-func main() {
-
 }
 
 /*
